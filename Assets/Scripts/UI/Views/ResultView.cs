37754de27ca1c329f0fb1d1dc212f7fde3b94c58
void2610 <izumisan2610@gmail.com>
2025-08-15 13:21:22 +0900

    結果演出修正

--- Assets/Scripts/UI/Views/ResultView.cs
+++ Assets/Scripts/UI/Views/ResultView.cs
@@ -18,26 +18,23 @@ public class ResultView : MonoBehaviour
     [SerializeField] private Image textBackgroundImage;
     
     [Header("勝敗背景色設定")]
-    [SerializeField] private Color playerWinColor = new Color(0f, 0.6f, 0f, 0.8f);  // プレイヤー勝利時の背景色（緑）
-    [SerializeField] private Color enemyWinColor = new Color(0.6f, 0f, 0f, 0.8f);   // 敵勝利時の背景色（赤）
+    [SerializeField] private Color playerWinColor;
+    [SerializeField] private Color enemyWinColor;
     
     private CanvasGroup _canvasGroup;
-    private CanvasGroup _textCanvasGroup;
+    private readonly Vector3 _textMoveStartPosition = new(-1000f, 0f, 0f); // テキストの初期位置
     
     private void Awake()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
-        _textCanvasGroup = textBackgroundImage.GetComponent<CanvasGroup>();
         
         // 初期状態は非表示
         _canvasGroup.alpha = 0f;
         _canvasGroup.interactable = false;
         _canvasGroup.blocksRaycasts = false;
-        _textCanvasGroup.alpha = 0f;
-        _textCanvasGroup.interactable = false;
-        _textCanvasGroup.blocksRaycasts = false;
         
         gavelImage.color = new Color(1f, 1f, 1f, 0f);
+        textBackgroundImage.transform.localPosition = _textMoveStartPosition;
     }
     
     /// <summary>
@@ -45,23 +42,25 @@ public class ResultView : MonoBehaviour
     /// </summary>
     public async UniTask ShowWinLoseResult(string result, bool isPlayerWin)
     {
-        await _canvasGroup.FadeIn(0.3f).ToUniTask();
+        await _canvasGroup.FadeIn(0.3f);
         
         // ガベルのアニメーション
         SeManager.Instance.PlaySe("Gavel");
-        await gavelImage.FadeIn(0.25f).ToUniTask();
+        await gavelImage.FadeIn(0.25f);
         await UniTask.Delay(1000);
-        await gavelImage.FadeOut(0.25f).ToUniTask();
+        await gavelImage.FadeOut(0.25f);
         
         await UniTask.Delay(1000);
         
         // テキストのアニメーション
         resultText.text = result;
         textBackgroundImage.color = isPlayerWin ? playerWinColor : enemyWinColor;
-        await _textCanvasGroup.FadeIn(0.3f).ToUniTask();
+        await textBackgroundImage.transform.MoveTo(Vector3.zero, 0.75f, Ease.InOutBack);
         await UniTask.Delay(3000); // 結果表示を2秒間維持
-        await _textCanvasGroup.FadeOut(0.3f).ToUniTask();
         
-        await _canvasGroup.FadeOut(0.3f).ToUniTask();
+        await _canvasGroup.FadeOut(0.3f);
+        
+        // 結果表示後の状態をリセット
+        textBackgroundImage.transform.localPosition = _textMoveStartPosition;
     }
 }
