737dd42be2182dad722a4d28a8fd0a55448657dd
void2610 <izumisan2610@gmail.com>
2025-08-24 12:24:01 +0900

    サウンド系初期化をRootに

--- Assets/Scripts/VContainer/RootLifetimeScope.cs
+++ Assets/Scripts/VContainer/RootLifetimeScope.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using Void2610.UnityTemplate;
 
 /// <summary>
 /// ルートレベルのLifetimeScope
@@ -8,11 +9,26 @@ using VContainer.Unity;
 /// </summary>
 public class RootLifetimeScope : LifetimeScope
 {
+    [SerializeField] private BgmManager bgmManager;
+    [SerializeField] private SeManager seManager;
+    
     protected override void Configure(IContainerBuilder builder)
     {
         // 設定管理とセーブデータ管理
         builder.Register<PersonalityLogService>(Lifetime.Singleton);
         builder.Register<SaveDataManager>(Lifetime.Singleton);
         builder.Register<SettingsManager>(Lifetime.Singleton);
+        
+        // シーン遷移管理
+        builder.Register<SceneTransitionService>(Lifetime.Singleton);
+        
+        // サウンドマネージャーの初期化
+        InitializeSoundManagers();
+    }
+    
+    private void InitializeSoundManagers()
+    {
+        DontDestroyOnLoad(Instantiate(bgmManager).gameObject);
+        DontDestroyOnLoad(Instantiate(seManager).gameObject);
     }
 }
