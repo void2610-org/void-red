765b46cd6e1fd4b7c5419181e84daa1ae602baa4
void2610 <izumisan2610@gmail.com>
2025-09-11 10:35:23 +0900

    webでは動作しないように

--- Assets/Scripts/Game/Services/DiscordService.cs
+++ Assets/Scripts/Game/Services/DiscordService.cs
@@ -1,29 +1,49 @@
 using System;
 using UnityEngine;
+
+#if !UNITY_WEBGL || UNITY_EDITOR
 using Discord.Sdk;
+#endif
 
 public class DiscordService : IDisposable
 {
+#if !UNITY_WEBGL || UNITY_EDITOR
     private const ulong CLIENT_ID = 1415132179377160262;
     
     private readonly Client _client;
     private readonly DiscordRichPresenceService _richPresenceService;
+#endif
     
-    public void SetSceneState(SceneType sceneType) => _richPresenceService.SetSceneState(sceneType);
-    public void SetState(string prefix, string details) => _richPresenceService.SetState(prefix, details);
+    public void SetSceneState(SceneType sceneType)
+    {
+#if !UNITY_WEBGL || UNITY_EDITOR
+        _richPresenceService?.SetSceneState(sceneType);
+#endif
+    }
+    
+    public void SetState(string prefix, string details)
+    {
+#if !UNITY_WEBGL || UNITY_EDITOR
+        _richPresenceService?.SetState(prefix, details);
+#endif
+    }
     
     public DiscordService()
     {
+#if !UNITY_WEBGL || UNITY_EDITOR
         _client = new Client();
         _client.SetApplicationId(CLIENT_ID);
         _richPresenceService = new DiscordRichPresenceService(_client);
         
         // 初期のリッチプレゼンス設定
         _richPresenceService.SetSceneState(SceneType.Title);
+#endif
     }
     
     public void Dispose()
     {
+#if !UNITY_WEBGL || UNITY_EDITOR
         _client?.Dispose();
+#endif
     }
 }
