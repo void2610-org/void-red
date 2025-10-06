5d2707de3d90f9492b8e3682c22d648c99cc7e21
void2610 <izumisan2610@gmail.com>
2025-10-06 09:25:49 +0900

    カード詳細にキーワード表示

--- Assets/Scripts/UI/Views/CardDetailView.cs
+++ Assets/Scripts/UI/Views/CardDetailView.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using R3;
+using System.Linq;
 
 /// <summary>
 /// カード詳細情報を表示するモーダルViewクラス
@@ -17,6 +18,7 @@ public class CardDetailView : MonoBehaviour
     [SerializeField] private TextMeshProUGUI attributeText;
     [SerializeField] private TextMeshProUGUI scoreMultiplierText;
     [SerializeField] private TextMeshProUGUI collapseThresholdText;
+    [SerializeField] private TextMeshProUGUI keywordsText;
     [SerializeField] private TextMeshProUGUI evolutionInfoText;
     
     public Observable<Unit> PlayButtonClicked => playButton.OnClickAsObservable();
@@ -59,11 +61,34 @@ public class CardDetailView : MonoBehaviour
         attributeText.text = $"属性: {cardData.Attribute.ToJapaneseName()}";
         scoreMultiplierText.text = $"スコア倍率: {cardData.ScoreMultiplier:F1}x";
         collapseThresholdText.text = $"崩壊閾値: {cardData.CollapseThreshold}";
-        
+
+        // キーワード情報
+        UpdateKeywordsInfo(cardData);
+
         // 進化情報
         UpdateEvolutionInfo(cardData);
     }
     
+    /// <summary>
+    /// キーワード情報を更新
+    /// </summary>
+    /// <param name="cardData">カードデータ</param>
+    private void UpdateKeywordsInfo(CardData cardData)
+    {
+        if (cardData.Keywords == null || cardData.Keywords.Count == 0)
+        {
+            keywordsText.text = "キーワード: なし";
+            return;
+        }
+
+        // キーワードを日本語名に変換してカンマ区切りで表示
+        var keywordNames = cardData.Keywords
+            .Where(k => k != KeywordType.None)
+            .Select(k => k.GetJapaneseName());
+
+        keywordsText.text = $"キーワード: {string.Join(", ", keywordNames)}";
+    }
+
     /// <summary>
     /// 進化情報を更新
     /// </summary>
