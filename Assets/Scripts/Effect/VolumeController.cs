c2dd01e9a20a8ecfa49d593a75ab4922f475d35e
void2610 <izumisan2610@gmail.com>
2025-10-08 16:21:30 +0900

    Update VolumeController.cs

--- Assets/Scripts/Effect/VolumeController.cs
+++ Assets/Scripts/Effect/VolumeController.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 using Void2610.UnityTemplate;
 
+[RequireComponent(typeof(Volume))]
 public class VolumeController : SingletonMonoBehaviour<VolumeController>
 {
-    [SerializeField] private GlobalVolume globalVolume;
+    private Volume _volume;
 
     protected override void Awake()
     {
         base.Awake();
+        _volume = this.GetComponent<Volume>();
     }
 }
