f5606b5a49a57044b63882af1fa785775e93caf3
void2610 <izumisan2610@gmail.com>
2025-10-12 16:57:51 +0900

    設定の開閉

---
+++ Assets/Scripts/UI/Views/SettingButtonView.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using R3;
+
+/// <summary>
+/// 設定ボタンのViewコンポーネント
+/// </summary>
+[RequireComponent(typeof(Button))]
+public class SettingButtonView : MonoBehaviour
+{
+    public Observable<Unit> OnButtonClicked => _onButtonClicked;
+
+    private Button _button;
+    private readonly Subject<Unit> _onButtonClicked = new();
+
+    private void Awake()
+    {
+        _button = this.GetComponent<Button>();
+        _button.onClick.AddListener(() => _onButtonClicked.OnNext(Unit.Default));
+    }
+
+    private void OnDestroy()
+    {
+        _onButtonClicked?.Dispose();
+    }
+}
