fae5a6447b6644eb7b1645d1cd49f0b87eff89f7
void2610 <izumisan2610@gmail.com>
2025-10-15 11:16:46 +0900

    敵の出し方選択の偏り

--- Assets/Scripts/Game/Presenters/Enemy.cs
+++ Assets/Scripts/Game/Presenters/Enemy.cs
@@ -1,11 +1,24 @@
+using System.Collections.Generic;
+using System.Linq;
+using Void2610.UnityTemplate;
+
 /// <summary>
 /// NPCクラス
 /// AIによって制御される対戦相手を表す
 /// </summary>
 public class Enemy : PlayerPresenter
 {
-    public Enemy(HandView handView, GameProgressService gameProgressService = null, int maxHandSize = 3) 
-        : base(handView, gameProgressService, maxHandSize) { }
+    private EnemyData _data;
+    
+    public Enemy(HandView handView, GameProgressService gameProgressService = null, int maxHandSize = 3) : base(handView, gameProgressService, maxHandSize) { }
+    
+    public void SetEnemyData(EnemyData data) => _data = data;
+
+    public PlayStyle DecidePlayStyle()
+    {
+        var all = _data.PlaystyleWeights.Keys.ToList();
+        return all.ChooseByWeight(p => _data.PlaystyleWeights[p]);
+    }
     
     /// <summary>
     /// 敵はセーブしない（オーバーライドして無効化）
