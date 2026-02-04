619982872e2b8e39a17c12a3c7661724960123cd
void2610 <izumisan2610@gmail.com>
2026-02-05 01:42:10 +0900

    fix pause anim

--- Assets/Scripts/UI/Views/BattlePauseView.cs
+++ Assets/Scripts/UI/Views/BattlePauseView.cs
@@ -1,8 +1,9 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using R3;
-using Cysharp.Threading.Tasks;
 using LitMotion;
+using LitMotion.Extensions;
 using Void2610.UnityTemplate;
 
 /// <summary>
@@ -18,11 +19,14 @@ public sealed class BattlePauseView : MonoBehaviour, IPauseView
     [SerializeField] private Button helpButton;
     [SerializeField] private Button optionButton;
     [SerializeField] private RectTransform panelRect;
+    [SerializeField] private LayoutGroup buttonsLayoutGroup;
 
     [Header("アニメーション設定")]
     [SerializeField] private float slideDuration = 0.3f;
     [SerializeField] private float hiddenX = -400f;
     [SerializeField] private float shownX;
+    [SerializeField] private float buttonSlideOffset = -50f;
+    [SerializeField] private float buttonStaggerDelay = 0.05f;
 
     public bool IsShowing { get; private set; }
     public Observable<Unit> OnResumeButtonClicked => resumeButton.OnClickAsObservable();
@@ -32,16 +36,43 @@ public sealed class BattlePauseView : MonoBehaviour, IPauseView
 
     private CanvasGroup _canvasGroup;
     private MotionHandle _slideHandle;
+    private List<MotionHandle> _buttonAnimHandles = new();
+    private Dictionary<Transform, Vector2> _buttonOriginalPositions = new();
+
+    public void Toggle()
+    {
+        if (IsShowing) Hide();
+        else Show();
+    }
 
     public void Show()
     {
-        Debug.Log("BattlePauseView: Show");
         Time.timeScale = 0;
         IsShowing = true;
         _slideHandle.TryCancel();
-        
+        _buttonAnimHandles.CancelAll();
+
         _slideHandle = panelRect.MoveToX(shownX, slideDuration, Ease.OutQuad, ignoreTimeScale: true);
         _canvasGroup.FadeIn(0.1f, ignoreTimeScale: true);
+
+        // ボタンの順次スライドアニメーション
+        buttonsLayoutGroup.ForEachChildWithDelay((child, _, delay) =>
+        {
+            var rect = child.GetComponent<RectTransform>();
+            var targetPos = _buttonOriginalPositions[child];
+            var startPos = new Vector2(targetPos.x + buttonSlideOffset, targetPos.y);
+
+            rect.anchoredPosition = startPos;
+
+            var handle = LMotion.Create(startPos, targetPos, slideDuration)
+                .WithDelay(delay)
+                .WithEase(Ease.OutQuad)
+                .WithScheduler(MotionScheduler.UpdateIgnoreTimeScale)
+                .BindToAnchoredPosition(rect)
+                .AddTo(rect.gameObject);
+
+            _buttonAnimHandles.Add(handle);
+        }, buttonStaggerDelay);
     }
 
     public void Hide()
@@ -49,25 +80,30 @@ public sealed class BattlePauseView : MonoBehaviour, IPauseView
         Time.timeScale = 1;
         IsShowing = false;
         _slideHandle.TryCancel();
-        
+        _buttonAnimHandles.CancelAll();
+
         _slideHandle = panelRect.MoveToX(hiddenX, slideDuration, Ease.InQuad, ignoreTimeScale: true);
         _canvasGroup.FadeOut(0.1f, ignoreTimeScale: true);
-    }
-    
-    public void Toggle()
-    {
-        if (IsShowing) Hide();
-        else Show();
     }
 
     private void Awake()
     {
         _canvasGroup = GetComponent<CanvasGroup>();
         panelRect.anchoredPosition = new Vector2(hiddenX, panelRect.anchoredPosition.y);
+
+        // ボタンの初期位置を保存
+        var buttonsParent = buttonsLayoutGroup.transform;
+        for (var i = 0; i < buttonsParent.childCount; i++)
+        {
+            var child = buttonsParent.GetChild(i);
+            var rect = child.GetComponent<RectTransform>();
+            _buttonOriginalPositions[child] = rect.anchoredPosition;
+        }
     }
 
     private void OnDestroy()
     {
         _slideHandle.TryCancel();
+        _buttonAnimHandles.CancelAll();
     }
 }
