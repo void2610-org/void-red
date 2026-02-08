ffacb40a395fabf04ecfd9b8e0f40378bfc75799
void2610 <izumisan2610@gmail.com>
2026-02-09 00:20:59 +0900

    Update ToolbarExtensionSelectedGameObject.cs

--- Assets/Editor/ToolbarExtensionSelectedGameObject.cs
+++ Assets/Editor/ToolbarExtensionSelectedGameObject.cs
@@ -4,48 +4,45 @@ using UnityEngine.EventSystems;
 using UnityEngine.UIElements;
 using YujiAp.UnityToolbarExtension.Editor;
 
-namespace GardenGnome.Editor
+/// <summary>
+/// ツールバーにEventSystemで選択中のGameObjectを表示
+/// </summary>
+public class ToolbarExtensionSelectedGameObject : IToolbarElement
 {
-    /// <summary>
-    /// ツールバーにEventSystemで選択中のGameObjectを表示
-    /// </summary>
-    public class ToolbarExtensionSelectedGameObject : IToolbarElement
+    public ToolbarElementLayoutType DefaultLayoutType => ToolbarElementLayoutType.RightSideLeftAlign;
+
+    private Label _label;
+
+    public VisualElement CreateElement()
     {
-        public ToolbarElementLayoutType DefaultLayoutType => ToolbarElementLayoutType.RightSideLeftAlign;
+        var container = new VisualElement();
+        container.style.flexDirection = FlexDirection.Row;
+        container.style.alignItems = Align.Center;
 
-        private Label _label;
+        // プレフィックスラベル
+        var prefixLabel = new Label("Selected: ");
+        prefixLabel.style.color = new Color(0.7f, 0.7f, 0.7f);
+        container.Add(prefixLabel);
 
-        public VisualElement CreateElement()
-        {
-            var container = new VisualElement();
-            container.style.flexDirection = FlexDirection.Row;
-            container.style.alignItems = Align.Center;
+        // 選択中オブジェクト名ラベル
+        _label = new Label("None");
+        _label.style.minWidth = 100;
+        _label.style.maxWidth = 200;
+        _label.style.overflow = Overflow.Hidden;
+        _label.style.textOverflow = TextOverflow.Ellipsis;
+        container.Add(_label);
 
-            // プレフィックスラベル
-            var prefixLabel = new Label("Selected: ");
-            prefixLabel.style.color = new Color(0.7f, 0.7f, 0.7f);
-            container.Add(prefixLabel);
+        // 更新ループを登録
+        EditorApplication.update += UpdateLabel;
 
-            // 選択中オブジェクト名ラベル
-            _label = new Label("None");
-            _label.style.minWidth = 100;
-            _label.style.maxWidth = 200;
-            _label.style.overflow = Overflow.Hidden;
-            _label.style.textOverflow = TextOverflow.Ellipsis;
-            container.Add(_label);
+        return container;
+    }
 
-            // 更新ループを登録
-            EditorApplication.update += UpdateLabel;
+    private void UpdateLabel()
+    {
+        if (_label == null) return;
 
-            return container;
-        }
-
-        private void UpdateLabel()
-        {
-            if (_label == null) return;
-
-            var selected = EventSystem.current?.currentSelectedGameObject;
-            _label.text = selected ? selected.name : "None";
-        }
+        var selected = EventSystem.current?.currentSelectedGameObject;
+        _label.text = selected ? selected.name : "None";
     }
 }
