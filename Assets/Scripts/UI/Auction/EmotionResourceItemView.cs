a18e5881139baad2a866aeea054c0532908a9292
void2610 <izumisan2610@gmail.com>
2026-02-08 16:49:49 +0900

    fix wheel

--- Assets/Scripts/UI/Auction/EmotionResourceItemView.cs
+++ Assets/Scripts/UI/Auction/EmotionResourceItemView.cs
@@ -6,16 +6,10 @@ using UnityEngine.UI;
 // 色インジケーター、残量テキスト、選択状態ボーダーを持つ
 public class EmotionResourceItemView : MonoBehaviour
 {
-    [SerializeField] private Image colorIndicator;
     [SerializeField] private TextMeshProUGUI amountText;
 
     public EmotionType Emotion { get; private set; }
 
     public void UpdateAmount(int amount) => amountText.text = amount.ToString();
-
-    public void Initialize(EmotionType emotion)
-    {
-        Emotion = emotion;
-        colorIndicator.color = emotion.GetColor();
-    }
+    public void Initialize(EmotionType emotion) => Emotion = emotion;
 }
