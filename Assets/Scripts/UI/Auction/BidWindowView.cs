200bfa384a31a74efac2ea66e5b730b07ff05b7e
void2610 <izumisan2610@gmail.com>
2026-02-23 23:23:56 +0900

    add bit text and fire

--- Assets/Scripts/UI/Auction/BidWindowView.cs
+++ Assets/Scripts/UI/Auction/BidWindowView.cs
@@ -7,8 +7,14 @@ using UnityEngine.UI;
 // カードクリック時にモーダル表示される入札額調整UI
 public class BidWindowView : BaseWindowView
 {
-    [Header("感情表示")]
+    [Header("情報表示")]
+    [SerializeField] private TMP_Text cardNameText;
+    [SerializeField] private TMP_Text emotionNameText;
     [SerializeField] private Image emotionIndicator;
+
+    [Header("炎演出")]
+    [SerializeField] private Image flameImage;
+    [SerializeField] private Sprite[] flameSprites;
 
     [Header("入札操作")]
     [SerializeField] private Button increaseButton;
@@ -19,7 +25,24 @@ public class BidWindowView : BaseWindowView
     public Observable<Unit> OnDecrease => decreaseButton.OnClickAsObservable();
     public Observable<Unit> OnClose => closeButton.OnClickAsObservable();
 
-    public void SetEmotion(EmotionType emotion) => emotionIndicator.color = emotion.GetColor();
+    public void SetCardName(string name) => cardNameText.text = name;
 
-    public void UpdateBidAmount(int amount) => bidAmountText.text = amount.ToString();
+    public void UpdateBidAmount(int amount)
+    {
+        bidAmountText.text = amount.ToString();
+        UpdateFlame(amount);
+    }
+
+    public void SetEmotion(EmotionType emotion)
+    {
+        emotionIndicator.color = emotion.GetColor();
+        emotionNameText.text = emotion.ToJapaneseName();
+    }
+
+    // 入札値に応じた炎スプライトに差し替え
+    private void UpdateFlame(int amount)
+    {
+        var index = Mathf.Clamp(amount, 0, flameSprites.Length - 1);
+        flameImage.sprite = flameSprites[index];
+    }
 }
